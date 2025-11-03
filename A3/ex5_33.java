a8039732a39bab61028966d55e8f0c9fbaa0e29a
Daniel Gago <danigagonavarro89@gmail.com>
2025-11-03 18:09:22 +0100

    Update ex5_33.java

--- A3/ex5_33.java
+++ A3/ex5_33.java
@@ -1,39 +1,17 @@
-import java.util.*;
-
-public class ex5_33 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Random rand = new Random();
-
-        int[][] array = new int[4][4];
-
-        int sum = 0;
-
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                array[i][j] = rand.nextInt(10);
-                
-                System.out.println(array[j][i] + " ");
+int[][] matriu = {
+            {1,2,3,4},
+            {5,6,7,8},
+            {9,10,11,12},
+            {13,14,15,16}
+        };
+        int col = 0;
+        while (col < 4) {
+            int suma = 0;
+            int fila = 0;
+            while (fila < 4) {
+                suma += matriu[fila][col];
+                fila++;
             }
-            System.out.println();
+            System.out.println("Suma columna " + col + ": " + suma);
+            col++;
         }
-
-        System.out.println("\n");
-
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                sum += array[j][i];
-                
-                System.out.print(array[j][i] + " ");
-            }
-            System.out.println();
-            System.out.println("La suma de columna fila és: " + sum);
-        }
-        
-        
-
-        System.out.println("\n");
-
-        sc.close();
-    }
-}
