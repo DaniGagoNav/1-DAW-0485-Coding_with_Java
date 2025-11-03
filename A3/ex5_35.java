2ac00051aa4d18e7d9a611b93247f8694a9c5e76
Daniel Gago <danigagonavarro89@gmail.com>
2025-11-03 18:11:21 +0100

    Create ex5_35.java

---
+++ A3/ex5_35.java
@@ -0,0 +1,11 @@
+int[][] matriu = {
+            {1,2,3,4},
+            {5,6,7,8},
+            {9,10,11,12},
+            {13,14,15,16}
+        };
+        int i = 0;
+        while (i < 4) {
+            System.out.println("Diagonal[" + i + "][" + i + "] = " + matriu[i][i]);
+            i++;
+        }
