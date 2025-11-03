d2cc5bfe2e9b89d551c5062e0ceb1e488f9f0615
Daniel Gago <danigagonavarro89@gmail.com>
2025-11-03 18:12:20 +0100

    Create ex5_36.java

---
+++ A3/ex5_36.java
@@ -0,0 +1,19 @@
+int[][] matriu = {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        };
+        int[] temp = matriu[0];
+        matriu[0] = matriu[2];
+        matriu[2] = temp;
+        System.out.println("Matriu després de l'intercanvi de files:");
+        fila = 0;
+        while (fila < 3) {
+            int columna = 0;
+            while (columna < 3) {
+                System.out.print(matriu[fila][columna] + " ");
+                columna++;
+            }
+            System.out.println();
+            fila++;
+        }
