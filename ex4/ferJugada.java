87456f874516080be6ab2798f62196455de6bb5d
Daniel Gago <danigagonavarro89@gmail.com>
2025-11-16 17:49:34 +0100

    Delete ex4 directory

--- ex4/ferJugada.java
+++
@@ -1,9 +0,0 @@
-public class ferJugada {
-    public static void ferJugada(int comptador, char torn) {
-        if (comptador % 2 == 0){
-				torn = 'O';				
-			}else{
-				torn = 'X';
-			}
-    }
-}
