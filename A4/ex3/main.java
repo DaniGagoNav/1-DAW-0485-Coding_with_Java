89523befb2958aed1e0885d742c8bf623df12ed2
Daniel Gago <danigagonavarro89@gmail.com>
2025-11-16 17:50:12 +0100

    Add files via upload

--- A4/ex3/main.java
+++ A4/ex3/main.java
@@ -4,27 +4,31 @@ import java.util.*;
 public class main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Scanner sc1 = new Scanner(System.in);
 
         int input = 0;
-        
         do{
             System.out.println("1.- SUMA VOCALS\n2.- INVERTEIX CADENA\n3.- ES PALINDROM?\n4.- SURT");
 
             System.out.println();
-
             System.out.println("Introdueix la acció que vols fer: ");
             input = sc.nextInt();
-
-        }while (input != 4); {
+            String inputText;
             switch (input) {
                 case 1:
-                    metode.comptaVocals("");
+                    System.out.println("Introdueix el text que vols posar: ");
+                    inputText = sc1.nextLine();
+                    metode.comptaVocals(inputText);
                     break;
                 case 2 :
-                    metode.inverteixText("");
+                    System.out.println("Introdueix el text que vols posar: ");
+                    inputText = sc1.nextLine();
+                    metode.inverteixText(inputText);
                     break;
                 case 3:
-                    metode.esPalindrom(true);
+                    System.out.println("Introdueix el text que vols posar: ");
+                    inputText = sc1.nextLine();
+                    metode.esPalindrom(inputText);
                     break;
                 case 4:
                     System.out.println("Has sortit!");
@@ -33,7 +37,7 @@ public class main {
                     System.out.println("Entrada invalida.");
                     break;    
             }
-        }
+        }while(input != 4);
         sc.close();
     }
 }
--- A4/ex3/metode.java
+++ A4/ex3/metode.java
@@ -2,13 +2,10 @@ package ex3;
 import java.util.*;
 
 public class metode {
-    public static void comptaVocals(String cadena1){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Inserta una cadena per comptar les vocals: ");
-        cadena1 = sc.nextLine();
+    public static void comptaVocals(String inputText){
         int comptador = 0;
-        for (int i = 0; i < cadena1.length(); i++){
-            char vocal = cadena1.charAt(i);
+        for (int i = 0; i < inputText.length(); i++){
+            char vocal = inputText.charAt(i);
             if (vocal == 'a' || vocal == 'e' || vocal == 'i' || vocal == 'o' || vocal == 'u' || vocal == 'A' || vocal == 'E' || vocal == 'I' || vocal == 'O' || vocal == 'U'){
                 comptador++;
             }
@@ -16,33 +13,23 @@ public class metode {
         System.out.println("El numero de vocals és: " + comptador);
     }
 
-    public static void inverteixText(String textOriginal){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Inserta una frase per invertirla: ");
-        textOriginal = sc.nextLine();
+    public static void inverteixText(String inputText){
         String textInvertit = "";
-        for (int i = textOriginal.length() -1; i >= 0; i--){
-            textInvertit += textOriginal.charAt(i);
+        for (int i = inputText.length() -1; i >= 0; i--){
+            textInvertit += inputText.charAt(i);
         }
         System.out.println("El text invertit és: " + textInvertit);
     }
 
-    public static void esPalindrom(boolean esPalindromBool){
-        Scanner sc = new Scanner(System.in);
+    public static void esPalindrom(String inputText){
+        String textMinus = inputText.toLowerCase();
+        boolean esPalindromBool = true;
 
-        String textPrincipal;
-        System.out.println("Inserta una frase per mostrar si es un palindrom: ");
-        textPrincipal = sc.nextLine();
-        String textMinus = textPrincipal.toLowerCase();
-        
-        esPalindromBool = true;
         for (int i = 0; i < textMinus.length()/2; i++){
             if (!(textMinus.charAt(i) == textMinus.charAt(textMinus.length()-i -1))) {
                 esPalindromBool = false;
             }
         }
         System.out.println(esPalindromBool);
-
-        sc.close();
     }
 }
